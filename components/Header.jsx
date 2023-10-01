97da8c603b35f423ca57d8e7f424050237170558
unknown <dmitriev_serega@mail.ru>
2023-10-01 20:08:24 +0500

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link"
+import styles from './Header.module.css';
+
+const pages = [
+    {href:'/', name:'На главную'},
+    {href:'/about', name:'О сайте'},
+    {href:'/contact', name:'Контакты'},
+];
+
+export default function Header(){
+    return <header>
+        <nav className={styles.nav}>
+            <ul>
+                {pages.map(({href, name})=>
+                <li key={href}>
+                    <Link href={href}>{name}</Link></li>)}
+                {/* {<li><Link href="/">Home</Link></li>
+                <li><Link href="/about">About</Link></li>} */}
+            </ul>
+        </nav>
+    </header>
+} 
